299c8201fd24706e0e2037ca1bcc45837dce66ab
Kevin Morales <kfmorales94@gmail.com>
2025-01-10 18:52:13 -0500

    Done

--- app/src/main/java/com/kevinhomorales/azuremapskotlin/MainActivity.kt
+++ app/src/main/java/com/kevinhomorales/azuremapskotlin/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.kevinhomorales.azuremapskotlin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.kevinhomorales.azuremapskotlin.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpView()
+    }
+
+    private fun setUpView() {
+        setUpMap()
+    }
+
+    fun setUpMap() {
+        // Configurar ajustes del WebView
+        val webSettings: WebSettings = binding.mapWebViewId.settings
+        webSettings.javaScriptEnabled = true           // Activar JavaScript
+        webSettings.domStorageEnabled = true           // Activar almacenamiento DOM
+        webSettings.useWideViewPort = true             // Usar el ancho completo de la vista
+        webSettings.loadWithOverviewMode = true        // Ajustar el contenido al tamaño de la pantalla
+
+        // Asegurarse de que la pantalla ocupa el espacio completo
+        binding.mapWebViewId.setInitialScale(1)         // Escalar el contenido adecuadamente
+        binding.mapWebViewId.layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+
+        // Cargar el archivo HTML
+        binding.mapWebViewId.loadUrl("file:///android_asset/map.html")
     }
 }
